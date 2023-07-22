c17be49d427f68491299e7562c7332fab282f195
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-07-22 16:05:20 +0300

    FIX: in card fix import Icon

--- src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,19 +1,13 @@
 import { NavLink } from 'react-router-dom';
 import Pet from '../../images/petPhoto/pet-photo-small.jpg';
-import Icons from '../../images/icons.svg';
-
-const Icon = ({ name, color, size }) => (
-  <svg className={`icon icon-${name}`} fill={color} width={size} height={size}>
-    <use xlinkHref={`${Icons}#icon-${name}`} />
-  </svg>
-);
+import Icon from '../../utils/Icon/Icon';
 
 const NoticeCategoryItem = () => {
   return (
     <div>
       <img src={Pet} alt="pet" />
       <button type="button">
-        <Icon name="heart-min" color="#54adff" size={18} />
+        <Icon name="heart" color="#ff520e" size={18} />
       </button>
       <NavLink to="/add-pet">Add pet</NavLink>
       <h3>Сute dog looking for a home</h3>
