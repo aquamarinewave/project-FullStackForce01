02f9b3c5b18763c7a0800a04843612e31e6f01c7
Maryna Medynska <maryna.medynska@gmail.com>
2023-07-22 15:58:05 +0300

    Add background

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div>Hello</div>;
+};
+
+export default Header;
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,18 +1,12 @@
 import { BackgroundImage } from './NotFoundPage.styled';
 import { PageHeader } from './NotFoundPage.styled';
 import { LinkBtn } from './NotFoundPage.styled';
-import Icons from '../../images/icons.svg'; // Path to your icons.svg
-
-const Icon = ({ name, color, size }) => (
-  <svg className={`icon icon-${name}`} fill={color} width={size} height={size}>
-    <use xlinkHref={`${Icons}#icon-${name}`} />
-  </svg>
-);
+import Icon from '../../utils/Icon'; // Path to your icons.svg
 
 const NotFoundPage = () => {
   return (
     <div>
-      <PageHeader>Ooops! This page not found :(</PageHeader>
+      <PageHeader>Ooops! This page not found :</PageHeader>
       <BackgroundImage></BackgroundImage>
       <LinkBtn to="/main">
         To main page <Icon name="pawprint-1" color="#FFFFFF" size={35} />
