30532e75642dfbe967e0b2c4c822756986f4b859
Maryna Medynska <maryna.medynska@gmail.com>
2023-07-22 18:13:52 +0300

    Working

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,16 +1,48 @@
-import { BackgroundImage } from './NotFoundPage.styled';
-import { PageHeader } from './NotFoundPage.styled';
+import { PageHeader, BackgroundImage } from './NotFoundPage.styled';
 import { LinkBtn } from './NotFoundPage.styled';
 import Icon from '../../utils/Icon/Icon'; // Path to your icons.svg
+import Smallpng from '../../images/x1/page404/404Small.png';
+import Small from '../../images/x1/page404/404Small.webp';
+import SmallRetina from '../../images/x2/page404/404Small.webp';
+import Medium from '../../images/x1/page404/404Medium.webp';
+import MediumRetina from '../../images/x2/page404/404Medium.webp';
+import Large from '../../images/x1/page404/404Large.webp';
+import LargeRetina from '../../images/x2/page404/404Large.png';
 
 const NotFoundPage = () => {
   return (
     <div>
-      <PageHeader>Ooops! This page not found :</PageHeader>
-      <BackgroundImage></BackgroundImage>
-      <LinkBtn to="/main">
-        To main page <Icon name="pawprint-1" color="#FFFFFF" size={35} />
-      </LinkBtn>
+      <BackgroundImage>
+        <PageHeader>Ooops! This page not found :</PageHeader>
+        <picture>
+          <source
+            media="(min-width: 1200px)"
+            srcSet={`${Small} 1x, 
+              ${SmallRetina} 2x
+        `}
+            type="image/webp"
+          />
+          <source
+            media="(min-width: 768px)"
+            srcSet={`${Medium} 1x,
+              ${MediumRetina} 2x
+        `}
+            type="image/webp"
+          />
+          <source
+            media="(max-width: 767px)"
+            srcSet={`${Large} 1x,
+              ${LargeRetina} 2x
+        `}
+            type="image/webp"
+          />
+          <img src={Smallpng} alt="error-404" />
+        </picture>
+
+        <LinkBtn to="/main">
+          To main page <Icon name="pawprint-1" color="#FFFFFF" size={35} />
+        </LinkBtn>
+      </BackgroundImage>
     </div>
   );
 };
--- src/pages/NotFoundPage/NotFoundPage.styled.js
+++ src/pages/NotFoundPage/NotFoundPage.styled.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import Small from '../../images/x1/page404/404Small.png';
-import SmallRetina from '../../images/x2/page404/404Small.png';
-import Medium from '../../images/x1/page404/404Medium.png';
-import MediumRetina from '../../images/x2/page404/404Medium.png';
-import Large from '../../images/x1/page404/404Large.png';
-import LargeRetina from '../../images/x2/page404/404Large.png';
+import Smallbg from '../../images/x1/bg/bgSmall.png';
+import SmallbgRetina from '../../images/x2/bg/bgSmall.png';
+import Mediumbg from '../../images/x1/bg/bgMedium.png';
+import MediumbgRetina from '../../images/x2/bg/bgMedium.png';
+import Largebg from '../../images/x1/bg/bgLagre.png';
+import LargebgRetina from '../../images/x2/bg/bgLarge.png';
 
 export const PageHeader = styled.h2`
+  padding-top: 50px;
   margin-bottom: 80px;
   font-size: 24px;
   font-weight: 700;
@@ -17,24 +18,22 @@ export const PageHeader = styled.h2`
 export const BackgroundImage = styled.div`
   background-repeat: no-repeat;
   background-position: center;
-  background-size: cover;
-  background-image: url(${Small});
-  height: 123px;
+  background-size: contain;
+  height: 1000px;
+  background-image: url(${Smallbg});
   @media (min-device-pixel-ratio: 2), (min-resolution: 192dpi), (min-resolution: 2dppx) {
-    background-image: url(${SmallRetina});
+    background-image: url(${SmallbgRetina});
   }
   @media screen and (min-width: 768px) {
-    background-image: url(${Medium});
-    height: 308px;
+    background-image: url(${Mediumbg});
     @media (min-device-pixel-ratio: 2), (min-resolution: 192dpi), (min-resolution: 2dppx) {
-      background-image: url(${MediumRetina});
+      background-image: url(${MediumbgRetina});
     }
   }
   @media screen and (min-width: 1280px) {
-    background-image: url(${Large});
-    height: 360px;
+    background-image: url(${Largebg});
     @media (min-device-pixel-ratio: 2), (min-resolution: 192dpi), (min-resolution: 2dppx) {
-      background-image: url(${LargeRetina});
+      background-image: url(${LargebgRetina});
     }
   }
 `;
