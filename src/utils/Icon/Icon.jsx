5701fd030c57530f29e9724ea385123e99e27463
Maryna Medynska <maryna.medynska@gmail.com>
2023-07-22 14:39:27 +0300

    Add component Icon

---
+++ src/utils/Icon/Icon.jsx
@@ -0,0 +1,9 @@
+import Icons from '../../images/icons.svg'; // Path to your icons.svg
+
+const Icon = ({ name, color, size }) => (
+  <svg className={`icon icon-${name}`} fill={color} width={size} height={size}>
+    <use xlinkHref={`${Icons}#icon-${name}`} />
+  </svg>
+);
+
+export default Icon;
