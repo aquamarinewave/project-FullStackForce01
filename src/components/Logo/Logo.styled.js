5f71c3e9c13f8cfd2002ebd90695f4d6540a2cfc
Maryna Medynska <maryna.medynska@gmail.com>
2023-07-23 23:21:28 +0300

    Delete space

--- src/components/Logo/Logo.styled.js
+++ src/components/Logo/Logo.styled.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
 const MainLogo = styled.img`
-margin:0;
-@media screen and(min - width: 768px) {
+  margin: 0;
+  @media screen and(min-width: 768px) {
     margin: 0;
   }
 
   @media screen and (min-width: 1280px) {
-margin-right: 80px;
-  }`
+    margin-right: 80px;
+  }
+`;
 
 export default MainLogo;
