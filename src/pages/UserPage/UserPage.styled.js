6bf09dfc3e7ad376f82b3a32d22560f8012863d5
Maryna Medynska <maryna.medynska@gmail.com>
2023-07-23 21:55:54 +0300

    Correct mistake path userpage

--- src/pages/UserPage/UserPage.styled.js
+++ src/pages/UserPage/UserPage.styled.js
@@ -1,7 +1,6 @@
-const { styled } = require('styled-components');
+import styled from 'styled-components';
 
 export const UserPageContainer = styled.div`
   display: flex;
-
   padding-top: 40px;
 `;
