86e555cc60aef62b4d7efcef0207f220d39c6bc4
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-07-24 20:33:12 +0300

    ADD: styles to NoticesItem, count date

--- src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -2,26 +2,43 @@ import { NavLink } from 'react-router-dom';
 import Icon from '../../utils/Icon/Icon';
 import React, { useState } from 'react';
 import { ModalNotice } from '../ModalNotice/ModalNotice';
-import { CategoriesContainer, Category, NoticesItemThumb } from './NoticeCategoryItem.styled';
+import { CategoriesContainer, CategoriesName, NoticesItemThumb, DiscriptionList } from './NoticeCategoryItem.styled';
 
 const NoticeCategoryItem = ({ responseByCategory }) => {
   // const [showModal, setShowModal] = useState(false);
   const [setShowModal] = useState(false);
 
   const { _id, comments, title, birthday, category, location, name, owner, sex, type, avatarURL } = responseByCategory;
+
+  console.log('birthday:', birthday);
   // const { title, category, avatarURL } = responseByCategory;
 
   const openModal = () => {
     setShowModal(showModal => !showModal);
   };
 
+  const givenDate = new Date(birthday);
+  const currentDate = new Date();
+  const timeDiff = currentDate - givenDate;
+  const millisecondsPerYear = 1000 * 60 * 60 * 24 * 365.25;
+  const yearsDiff = Math.floor(timeDiff / millisecondsPerYear);
+
   return (
     <div>
       <NoticesItemThumb>
         <img src={avatarURL} alt="pets avatar" />
         <CategoriesContainer>
-          <Category>{category}</Category>
+          {category !== 'for-free' ? (
+            <CategoriesName>{category}</CategoriesName>
+          ) : (
+            <CategoriesName>in goood hands</CategoriesName>
+          )}
         </CategoriesContainer>
+        <DiscriptionList>
+          <li>{location}</li>
+          {yearsDiff === 1 ? <li>{`${yearsDiff} year`}</li> : <li>{`${yearsDiff} years`}</li>}
+          <li>{sex}</li>
+        </DiscriptionList>
         <button type="button">
           <Icon name="heart" color="var(--dark-blue)" size={18} />
         </button>
--- src/components/NoticeCategoryItem/NoticeCategoryItem.styled.js
+++ src/components/NoticeCategoryItem/NoticeCategoryItem.styled.js
@@ -10,12 +10,23 @@ const CategoriesContainer = styled.div`
   justify-content: center;
   align-items: center;
   background-color: var(--lightblue);
+  padding: 10px 16px;
   height: 36px;
   border-top-right-radius: 16px;
   border-bottom-right-radius: 16px;
-  top: 34px;
+  top: 17px;
 `;
 
-const Category = styled.span``;
+const CategoriesName = styled.span`
+  font-size: 14px;
+  font-weight: 500;
+`;
 
-export { NoticesItemThumb, Category, CategoriesContainer };
+const DiscriptionList = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+`;
+
+export { NoticesItemThumb, CategoriesName, CategoriesContainer, DiscriptionList };
