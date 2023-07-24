b12a52f6fd6e2515b2b694223551d7680492c673
Inna Kukla <0969125133@ukr.net>
2023-07-24 15:52:14 +0300

    Update LoginPage.jsx

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,7 @@
-import AuthFormLogin from "components/AuthForm/AuthFormLogin"
+import AuthFormLogin from 'components/AuthForm/AuthFormLogin';
 
 function LoginPage() {
-  return (
-    <AuthFormLogin/>
-  )
+  return <AuthFormLogin />;
 }
 
 export default LoginPage;
