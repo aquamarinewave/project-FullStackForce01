916e6977868eb9dc401b39b9d59d57ac0b809eff
Kateryna Kasian <kkasyan1@gmail.com>
2023-07-24 13:21:06 +0300

    made logo optimisation

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,34 +1,43 @@
-import LogoBig from '../../images/x1/logo/LogoDefault.png';
-import LogoSmall from '../../images/x1/logo/LogoSmall.png';
+import Largepng from '../../images/x1/logo/LogoDefault.png';
+import Large from '../../images/x1/logo/LogoDefault.webp'
+import LargeRetina from '../../images/x2/logo/LogoDefault.webp'
+import Smallpng from '../../images/x1/logo/LogoSmall.png';
+import Small from '../../images/x1/logo/LogoSmall.webp';
+import SmallRetina from '../../images/x2/logo/LogoSmall.webp';
+
 import { NavLink } from 'react-router-dom';
 import { MainLogo } from './Logo.styled';
 
-// const handleImageSrc = src => {
-//   switch (src) {
-//     case 'big':
-//           return `${ LogoImageBig }`;
-//     case 'small':
-//       return `${LogoImageSmall}`;
-//     default:
-//       return LogoImageSmall;
-//   }
-// };
-
-// const image =  ({ src }) => handleImageSrc(src);
-
-// const Logo = ({width, src}) => {
-//     return (<MainLogo src={LogoImageBig} alt={'Logo'} />)
-// }
-
-// export default Logo;
+const isDesktopOrTablet = window.screen.width > 768;
+const isMobile = window.screen.width < 768;
 
 const Logo = () => {
   return (
     <NavLink to="/">
-      <picture>
-        <source srcSet={LogoSmall} media="(max-width: 767px)" />
-        <MainLogo src={LogoBig} alt="Logo" />
-      </picture>
+      {isDesktopOrTablet && (
+        <picture>
+          <source
+            media="(min-width: 768px)"
+            srcSet={`${Large} 1x, 
+              ${LargeRetina} 2x
+        `}
+            type="image/webp"
+          />
+          <MainLogo src={Largepng} alt="Logo" />
+        </picture>
+      )}
+      {isMobile && (
+        <picture>
+          <source
+            media="(min-width: 768px)"
+            srcSet={`${Small} 1x, 
+              ${SmallRetina} 2x
+        `}
+            type="image/webp"
+          />
+          <MainLogo src={Smallpng} alt="Logo" />
+        </picture>
+      )}
     </NavLink>
   );
 };
