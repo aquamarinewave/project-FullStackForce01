2c67ee55c0d993348cdf73691cbf4c831bf7b0fe
Igor Sedura <igorsedura@gmail.com>
2023-07-25 20:30:04 +0300

    user info from backend

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,5 +1,7 @@
 import { Formik, Form } from 'formik';
 import * as React from 'react';
+import { useSelector } from 'react-redux';
+import { userSelector } from '../../redux/auth/authSelector';
 
 import {
   ProfileInfo,
@@ -17,12 +19,22 @@ import avatarDefault from '../../images/profilephotos/avatar-default.png';
 import sprite from '../../images/icons.svg';
 
 const UserForm = () => {
+  const user = useSelector(userSelector);
+  const initialValues = {
+    avatarURL: user?.avatarURL || { avatarDefault },
+    name: user?.name || 'Enter your name',
+    email: user?.email || 'example@mail.com',
+    phone: user?.phone || '+380000000000',
+    birthday: user?.birthday || '01.01.2000',
+    city: user?.city || 'Kiev',
+  };
+
   return (
     <>
       <WrapperCard>
         <ProfileTitle>My information:</ProfileTitle>
         <ProfileInfo>
-          <Formik>
+          <Formik initialValues={initialValues}>
             <Form>
               <AvatarWrapper>
                 <EditButton>
@@ -30,32 +42,32 @@ const UserForm = () => {
                     <use href={`${sprite}#icon-edit-2`}></use>
                   </IconEdit>
                 </EditButton>
-                <ImgAvatar src={avatarDefault} alt="avatar" />
+                <ImgAvatar src={initialValues.avatarURL} alt="avatar" />
               </AvatarWrapper>
 
               <WrapperField>
-                <Label htmlFor="text"> Name:</Label>
+                <Label htmlFor="name"> Name:</Label>
 
-                <ProfileField type="text" name="text" placeholder="Enter your name" readOnly={true} />
+                <ProfileField type="text" name="name" placeholder={initialValues.name} readOnly={true} />
               </WrapperField>
               <WrapperField>
                 <Label htmlFor="email"> Email:</Label>
-                <ProfileField type="email" name="email" placeholder="example@mail.com" readOnly={true} />
+                <ProfileField type="email" name="email" placeholder={initialValues.email} readOnly={true} />
               </WrapperField>
               <WrapperField>
                 <Label htmlFor="date"> Birthday:</Label>
-                <ProfileField type="numder" name="birthday" placeholder="01.01.2000" readOnly={true} />
+                <ProfileField type="numder" name="birthday" placeholder={initialValues.birthday} readOnly={true} />
               </WrapperField>
 
               <WrapperField>
                 <Label htmlFor="phone"> Phone:</Label>
 
-                <ProfileField placeholder="+380000000000" type="phone" name="phone" readOnly={true} />
+                <ProfileField placeholder={initialValues.phone} type="phone" name="phone" readOnly={true} />
               </WrapperField>
 
               <WrapperField>
-                <Label htmlFor="email"> City:</Label>
-                <ProfileField type="email" name="email" placeholder="Kyiv" readOnly={true} />
+                <Label htmlFor="city"> City:</Label>
+                <ProfileField type="text" name="city" placeholder={initialValues.city} readOnly={true} />
               </WrapperField>
             </Form>
           </Formik>
