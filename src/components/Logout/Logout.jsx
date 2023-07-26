dac5cd2ea2896107f886dc85070f692691d43484
Kateryna Kasian <kkasyan1@gmail.com>
2023-07-26 19:06:00 +0300

    modalApproveAction opens using logout button

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,21 +1,38 @@
+
+import {useState } from 'react';
 import { useDispatch } from 'react-redux';
 import authOperations from 'redux/auth/operations';
 import { Button, ButtonText, IconLogout } from 'components/Logout/Logout.styled';
 import sprite from '../../images/icons.svg';
+import Modal from 'components/ModalApproveAction/ModalApproveAction';
 
-const Logout = ({isMobile}) => {
+const Logout = ({ isMobile }) => {
   const dispatch = useDispatch();
-  const onLogout = () => dispatch(authOperations.logoutUser());
+    const onLogout = () => dispatch(authOperations.logoutUser());
+      const [isModalOpen, setIsModalOpen] = useState(false);
 
+    const toggleModal = () => {
+    setIsModalOpen(prevState => !prevState);
+    };
+    
   return (
-    <Button onClick={onLogout} isMobile={isMobile}>
-      <ButtonText color="logout" weight="bold" margin="8px">
-        Logout
-      </ButtonText>
-      <IconLogout width={24} height={24}>
-        <use href={`${sprite}#icon-logout`}></use>
-      </IconLogout>
-    </Button>
+    <>
+          <Button onClick={toggleModal}
+            //   onClick={onLogout}
+              isMobile={isMobile}>
+        <ButtonText color="logout" weight="bold" margin="8px">
+          Logout
+        </ButtonText>
+        <IconLogout width={24} height={24}>
+          <use href={`${sprite}#icon-logout`}></use>
+        </IconLogout>
+      </Button>
+      {isModalOpen && (
+        <Modal isOpen={isModalOpen} toggleModal={toggleModal} onApprove={onLogout}>
+            Already leaving?
+        </Modal>
+      )}
+    </>
   );
 };
 
