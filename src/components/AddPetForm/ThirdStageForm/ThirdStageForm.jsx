2fb45041d982f01825fe5755e4219cc1818c58b9
Maryna Medynska <maryna.medynska@gmail.com>
2023-07-27 22:49:37 +0300

    comment unuse var

--- src/components/AddPetForm/ThirdStageForm/ThirdStageForm.jsx
+++ src/components/AddPetForm/ThirdStageForm/ThirdStageForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { BoxInputField, FieldLabel, InputField } from '../SecondStageForm/SecondStageForm.styled';
 import {
   BoxPetAvatar,
