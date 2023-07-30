1dfdbea88e676dcb6ecf714a37bfc9d1962820f9
Maryna Medynska <maryna.medynska@gmail.com>
2023-07-30 19:03:01 +0300

    Correct files Patination ti Pagitation.styled

--- src/components/Pagination/Pagination.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Pagination as MuiPagination } from '@mui/material';
-
-const Pagination = ({ page, count, onChange }) => {
-  return <MuiPagination page={page} count={count} variant="outlined" onChange={onChange} />;
-};
-
-export default Pagination;
