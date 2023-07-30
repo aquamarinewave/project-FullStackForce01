916dde90b1d1896329fa76cbf8957ffeefb6e4f1
Woodiik <vovikvova04@gmail.com>
2023-07-31 00:04:42 +0300

    Update Pagination.jsx

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Pagination as MuiPagination } from '@mui/material';
 import { PaginationList } from './Pagination.styled.';
 
 const Pagination = ({ page, count, onChange }) => {
