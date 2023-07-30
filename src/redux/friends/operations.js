4df36d8492e61e936a7a91eb22f2b2b724cb0d4c
Maryna Medynska <maryna.medynska@gmail.com>
2023-07-30 20:33:58 +0300

    friends redux

---
+++ src/redux/friends/operations.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://fullstackforce.onrender.com/api';
+
+const fetchFriends = createAsyncThunk('friends/fetch', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get(`/friends`);
+    return response.data[0];
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+const friendsOperations = {
+  fetchFriends,
+};
+
+export default friendsOperations;
