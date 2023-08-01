9e666c7ff91c5d8375e2c6a0fb46c5c1c8ed255d
Vladyslav Simak <simakv21@gmail.com>
2023-08-01 21:03:12 +0300

    add attention to AddPet

--- src/components/AddPetButton/AddPetButton.jsx
+++ src/components/AddPetButton/AddPetButton.jsx
@@ -1,14 +1,35 @@
 import sprite from '../../images/icons.svg';
 import { AddPet, IconPlusSmall } from './AddPetButton.styled';
+import ModalAttention from '../ModalAttention/ModalAttention';
+import authSelector from '../../redux/auth/authSelector';
+import React, { useState } from 'react';
+
+import { useSelector } from 'react-redux';
 
 const AddPetButton = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const isLoggedIn = useSelector(authSelector.loggedInSelector);
+
+  const handleLinkClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <AddPet to="/add-pet">
+    <>
+    <AddPet to={isLoggedIn && "/add-pet"} onClick={handleLinkClick}>
       Add Pet
       <IconPlusSmall width={24} height={24}>
         <use href={`${sprite}#icon-plus-small`}></use>
       </IconPlusSmall>
     </AddPet>
+
+    {!isLoggedIn && isModalOpen && (<ModalAttention onClose={closeModal}/>)}
+    </>
   );
 };
 
