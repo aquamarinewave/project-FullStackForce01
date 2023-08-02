309de573232fab8b9d9a64edaaa26baecb0443b3
Maryna Medynska <maryna.medynska@gmail.com>
2023-08-03 00:08:47 +0300

    Add style to pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
 import { PaginationList } from './Pagination.styled.';
+import { PaginationItem } from '@mui/material';
+import KeyboardBackspaceOutlined from '@mui/icons-material/KeyboardBackspaceOutlined';
+import TrendingFlatOutlined from '@mui/icons-material/TrendingFlatOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#54ADFF',
+    },
+    secondary: {
+      main: '#FEF9F9',
+    },
+  },
+});
 
 const Pagination = ({ page, count, onChange }) => {
-  return <PaginationList page={page} count={count} variant="outlined" onChange={onChange} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <PaginationList
+        page={page}
+        count={count}
+        variant="outlined"
+        onChange={onChange}
+        renderItem={item => (
+          <PaginationItem
+            sx={{
+              '&.Mui-selected': {
+                background: '#54ADFF',
+                color: '#FEF9F9',
+                border: 'none',
+              },
+              borderColor: '#54ADFF',
+              fill: '#54ADFF',
+              stroke: '#54ADFF',
+            }}
+            slots={{ previous: KeyboardBackspaceOutlined, next: TrendingFlatOutlined }}
+            {...item}
+          />
+        )}
+      />
+    </ThemeProvider>
+  );
 };
 
 export default Pagination;
