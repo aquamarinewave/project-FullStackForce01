02398a2c6d161a3d92c9c67fc7283f2942e88267
Maryna Medynska <maryna.medynska@gmail.com>
2023-08-02 22:43:39 +0300

    redux to new sintax

--- src/redux/news/newsSlice.js
+++ src/redux/news/newsSlice.js
@@ -1,31 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit';
 import newsOperations from './operations';
 
-const newsSlice = createSlice({
+const newsList = {
+  items: [],
+  error: null,
+  currentPage: 1,
+  pattern: '',
+  perPage: 6,
+  totalPages: 0,
+};
+
+const handleFulfilledNews = (state, action) => {
+  state.error = null;
+  const { news, totalCount } = action.payload;
+  state.items = news.sort((a, b) => new Date(b.date) - new Date(a.date));
+  state.totalPages = Math.ceil(totalCount / state.perPage);
+};
+
+const handleFulfilledPattern = (state, action) => {
+  state.error = null;
+  state.pattern = action.payload;
+};
+
+const handleFulfille = (state, action) => {
+  state.error = null;
+  state.currentPage = action.payload;
+};
+
+export const newsSlice = createSlice({
   name: 'news',
-  initialState: {
-    items: [],
-    error: null,
-    currentPage: 1,
-    pattern: '',
-    perPage: 6,
-    totalPages: 0,
-  },
-  extraReducers: {
-    [newsOperations.fetchNews.fulfilled](state, action) {
-      state.error = null;
-      const { news, totalCount } = action.payload;
-      state.items = news.sort((a, b) => new Date(b.date) - new Date(a.date));
-      state.totalPages = Math.ceil(totalCount / state.perPage);
-    },
-    [newsOperations.setPattern.fulfilled](state, action) {
-      state.error = null;
-      state.pattern = action.payload;
-    },
-    [newsOperations.setCurrentPage.fulfilled](state, action) {
-      state.error = null;
-      state.currentPage = action.payload;
-    },
+  initialState: newsList,
+
+  extraReducers: builder => {
+    builder
+      .addCase(newsOperations.fetchNews.fulfilled, handleFulfilledNews)
+      .addCase(newsOperations.setPattern.fulfilled, handleFulfilledPattern)
+      .addCase(newsOperations.setCurrentPage.fulfilled, handleFulfille);
   },
 });
 
